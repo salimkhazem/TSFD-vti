47c6b1cc5058e23c0fb1f9a8d330db73a6cb3d6f
Salim Khazem Macbook-Pro <salimkhazem97@gmail.com>
2024-07-04 15:20:27 +0200

    Adding main script

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys 
+import yaml 
+import torch 
+import pathlib 
+from utils import utils
+from models import model 
+from data import data_loader
+from training import engine 
+
+with open(sys.argv[1]) as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+model = model.UNet(n_channels=1, n_classes=1).to(device)
+optimizer = utils.get_optimizer(cfg, model)
+criterion = utils.get_criterion(cfg)
+train_dataset, valid_dataset, train_paths, valid_paths = data_loader.create_datasets(
+        cfg["Dataset"]["args"]["root_dir"], validation_split=0.2
+    )
+train_loader, valid_loader = data_loader.create_dataloaders(
+        train_dataset, valid_dataset
+    )
+logdir = pathlib.Path("logs")
+logdir = utils.generate_unique_logpath(logdir, "experiment_1")
+engine.train_one_epoch(model, criterion, optimizer, train_loader, device)
+
+
