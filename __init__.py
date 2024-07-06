0c44dce9a91feb2fdc4c28d47984070c0b9bd4fe
Salim Khazem Shire <salim.khazem@centralesupelec.fr>
2024-07-06 04:53:52 +0200

    Init

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from time import strftime
-from .utils.logs import create_logger 
+from utils.logs import create_logger 
 
 def init_logger(date_suffix=None, **kwargs): 
     date = strftime("%Y%m%d")
     time = strftime("%Hh%Mmin%Ss")
-    logger, logger_file, path_logs, final_dest = create_logger(
+    logger, logger_file, path_logs= create_logger(
         logger_name="main", date=date, time=time, date_suffix=date_suffix, **kwargs
     )
-    return logger, date, time, path_logs, final_dest 
+    return logger, date, time, path_logs
