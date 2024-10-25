843e742cf96ec09782b54b10d410904defd22bb2
Salim Khazem Shire <salim.khazem@centralesupelec.fr>
2024-10-25 03:06:42 +0200

    Update engine

--- training/engine.py
+++ training/engine.py
@@ -1,6 +1,7 @@
 import torch
 import logging 
 from tqdm.auto import tqdm 
+from sklearn import metrics
 
 logger_file = logging.getLogger('main_file')
 
@@ -8,7 +9,7 @@ logger_file = logging.getLogger('main_file')
 def train_one_epoch(model, criterion, optimizer, data_loader, grad_norm, device):
     if device is None: 
         device = next(model.parameters()).device 
-    loss_model, acc_model = 0, 0 
+    loss_model, acc_model, total_f1_score = 0, 0, 0 
     model.train()
     with tqdm(data_loader, total=len(data_loader.dataset), desc=f"Training", leave=False) as pbar: 
         for batch in pbar:
@@ -20,6 +21,10 @@ def train_one_epoch(model, criterion, optimizer, data_loader, grad_norm, device)
             optimizer.zero_grad()
             output = model(images) 
             acc_model += (output.argmax(1) == targets).sum().item()
+            y_true = targets.cpu().numpy().flatten()
+            y_pred = output.argmax(1).cpu().numpy().flatten()
+            batch_f1_score = metrics.f1_score(y_true, y_pred, average="macro", zero_division=1)
+            total_f1_score += batch_f1_score * images.size(0)  # Multiply by batch size to weight the average
             loss = criterion(output, targets)
             loss_model += loss.item()
             loss.backward()
@@ -28,7 +33,9 @@ def train_one_epoch(model, criterion, optimizer, data_loader, grad_norm, device)
             optimizer.step()
             pbar.set_postfix(**{"train_loss": loss.item()})
             pbar.update(images.shape[0])
-    return loss_model/len(data_loader.dataset), acc_model/len(data_loader.dataset)
+            del images, targets, output
+            torch.cuda.empty_cache()
+    return loss_model/len(data_loader.dataset), acc_model/len(data_loader.dataset), batch_f1_score, total_f1_score/len(data_loader.dataset)
 
 
 
@@ -39,14 +46,24 @@ def evaluate(model, criterion, data_loader, device):
     model.eval()
     total_loss = 0
     correct_pred = 0 
+    total_f1_score = 0
     with tqdm(data_loader, total=len(data_loader.dataset), desc=f"Validation", leave=False) as pbar: 
         for batch in pbar:
             images, targets = batch["input"].to(device), batch["target"].to(device) 
-            assert images.shape[1] == model.in_channels, f"Expected {model.in_channels} channels, got {images.shape[0]}" 
+            try: 
+                assert images.shape[1] == model.in_channels, f"Expected {model.in_channels} channels, got {images.shape[0]}" 
+            except: 
+                assert images.shape[1] == model.module.in_channels, f"Expected {model.module.in_channels} channels, got {images.shape[0]}" 
             output = model(images)
             loss = criterion(output, targets)
             correct_pred += (output.argmax(1) == targets).sum().item()
+            y_true = targets.cpu().numpy().flatten()
+            y_pred = output.argmax(1).cpu().numpy().flatten()
+            batch_f1_score = metrics.f1_score(y_true, y_pred, average="macro", zero_division=1)
+            total_f1_score += batch_f1_score * images.size(0)  # Multiply by batch size to weight the average
             total_loss += loss.item()
             pbar.set_postfix(**{"eval_loss": loss.item()})
             pbar.update(images.shape[0])
-    return total_loss/len(data_loader.dataset), correct_pred/len(data_loader.dataset)
+            del images, targets, output
+            torch.cuda.empty_cache()
+    return total_loss/len(data_loader.dataset), correct_pred/len(data_loader.dataset), batch_f1_score, total_f1_score/len(data_loader.dataset)
